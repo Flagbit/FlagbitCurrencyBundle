c4aa893db429d5f7d1e2741354d442c582aa583f
David Fuhr <david.fuhr@flagbit.de>
2014-04-17 11:27:50 +0200

    Added functional tests for twig extension

---
+++ Tests/Twig/FlagbitCurrencyExtensionTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Flagbit\Bundle\CurrencyBundle\Tests\Twig;
+
+use Flagbit\Bundle\CurrencyBundle\Service\Currency;
+use Flagbit\Bundle\CurrencyBundle\Twig\FlagbitCurrencyExtension;
+use Symfony\Component\Intl\Intl;
+use Twig_Test_IntegrationTestCase;
+
+class FlagbitCurrencyExtensionTest extends Twig_Test_IntegrationTestCase
+{
+    public function getExtensions()
+    {
+        return array(
+            new FlagbitCurrencyExtension(new Currency(Intl::getCurrencyBundle(), 'EUR')),
+        );
+    }
+
+    public function getFixturesDir()
+    {
+        return dirname(__FILE__) . '/Fixtures/';
+    }
+}
