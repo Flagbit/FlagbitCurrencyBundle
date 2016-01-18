a66c79acb301ae1a187a3504bea1b657e3064739
Claudio Zizza <zizza@flagbit.de>
2016-01-18 13:07:23 +0100

    Add assertion for flagbit_currency.twig Twig extension service

--- Tests/DependencyInjection/FlagbitCurrencyExtensionTest.php
+++ Tests/DependencyInjection/FlagbitCurrencyExtensionTest.php
@@ -15,12 +15,17 @@ class FlagbitCurrencyExtensionTest extends \PHPUnit_Framework_TestCase
         $extension = new FlagbitCurrencyExtension();
         $extension->load($config, $container);
 
+        $definition = $container->findDefinition('flagbit_currency.twig');
+        $definition->setPublic(true);
+
         $container->compile();
 
         $currencyBundle = $container->get('flagbit_currency.intl_bundle');
         $currency = $container->get('flagbit_currency');
+        $currencyExtension = $container->get('flagbit_currency.twig');
 
         $this->assertInstanceOf('Symfony\Component\Intl\ResourceBundle\CurrencyBundleInterface', $currencyBundle);
         $this->assertInstanceOf('Flagbit\Bundle\CurrencyBundle\Service\Currency', $currency);
+        $this->assertInstanceOf('Flagbit\Bundle\CurrencyBundle\Twig\FlagbitCurrencyExtension', $currencyExtension);
     }
 }
