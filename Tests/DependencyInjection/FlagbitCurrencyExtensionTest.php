cfce388fe5d759e0e54a143ebce25d4b12b58dfa
Claudio Zizza <zizza@flagbit.de>
2018-07-20 15:30:23 +0200

    Update service definition for autowire

--- Tests/DependencyInjection/FlagbitCurrencyExtensionTest.php
+++ Tests/DependencyInjection/FlagbitCurrencyExtensionTest.php
@@ -7,6 +7,7 @@ use Flagbit\Bundle\CurrencyBundle\Service\Currency;
 use Flagbit\Bundle\CurrencyBundle\Twig\FlagbitCurrencyExtension as TwigExtension;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\Intl\ResourceBundle\CurrencyBundleInterface;
 
 class FlagbitCurrencyExtensionTest extends TestCase
@@ -19,17 +20,32 @@ class FlagbitCurrencyExtensionTest extends TestCase
         $extension = new FlagbitCurrencyExtension();
         $extension->load($config, $container);
 
-        $definition = $container->findDefinition('flagbit_currency.twig');
-        $definition->setPublic(true);
+        $this->setDefinitionPublic(TwigExtension::class, $container);
+        $this->setDefinitionPublic(Currency::class, $container);
 
         $container->compile();
 
+        $currency = $container->get(Currency::class);
+        $currencyExtension = $container->get(TwigExtension::class);
+
+        $this->assertInstanceOf(Currency::class, $currency);
+        $this->assertInstanceOf(TwigExtension::class, $currencyExtension);
+
         $currencyBundle = $container->get('flagbit_currency.intl_bundle');
         $currency = $container->get('flagbit_currency');
-        $currencyExtension = $container->get('flagbit_currency.twig');
 
         $this->assertInstanceOf(CurrencyBundleInterface::class, $currencyBundle);
         $this->assertInstanceOf(Currency::class, $currency);
         $this->assertInstanceOf(TwigExtension::class, $currencyExtension);
+    }
+
+    /**
+     * @param string           $serviceName
+     * @param ContainerBuilder $container
+     */
+    private function setDefinitionPublic(string $serviceName, ContainerBuilder $container)
+    {
+        $container->findDefinition($serviceName)
+            ->setPublic(true);
     }
 }
